5c6f991fb3b9a62eed0db7a6153ce857f92ed393
Sarah DeBey <sarah.debey@gmail.com>
2016-04-26 08:58:30 -0700

    displays three random photos with sort of a counter

--- JS/app.js
+++ JS/app.js
@@ -4,10 +4,9 @@ var allProducts = [];
 function Image(name, filepath) {
   this.name = name;
   this.filepath = filepath;
-  var numShown = 0;
-  var numClicked = 0;
+  this.numShown = 0;
+  this.numClicked = 0;
   allProducts.push(this);
-  console.log(allProducts);
 };
 
 var bag = new Image('bag', 'img/bag.jpg');
@@ -31,37 +30,78 @@ var usb = new Image('usb', 'img/usb.jpg');
 var watercan = new Image('watercan', 'img/water-can.jpg');
 var wineglass = new Image('wineglass', 'img/wine-glass.jpg');
 
-function handleClicks () {
-  var parentNode = document.getElementById('image-container');
-  while (parentNode.firstChild) {
-    parentNode.removeChild(parentNode.firstChild);
-  };
+var imageNodeOne = document.getElementById('image-one');
+var imageNodeTwo = document.getElementById('image-two');
+var imageNodeThree = document.getElementById('image-three');
+var parentEl = document.getElementById('image-container');
+
+function makeOnePic () {
   var randomNumber = Math.floor(Math.random() * 20);
   var chosenProduct = allProducts[randomNumber].filepath;
-  var parentEl = document.getElementById('image-container');
-  var imgEl = document.createElement('img');
-  imgEl.src = chosenProduct;
-  parentEl.appendChild(imgEl);
+  allProducts[randomNumber].numShown += 1;
+  imageNodeOne.src = chosenProduct;
+
+}
+
+function displayPics () {
+
+  imageNodeOne.src = '';
+  imageNodeTwo.src = '';
+  imageNodeThree.src = '';
+  makeOnePic();
+  // var randomNumber = Math.floor(Math.random() * 20);
+  // var chosenProduct = allProducts[randomNumber].filepath;
+  //
+  // allProducts[randomNumber].numShown += 1;
+  // console.log(allProducts[randomNumber].name + (allProducts[randomNumber].numShown));
+
+  // var imgEl = document.createElement('img');
+  // imageNodeOne = document.getElementById('image-one');
+  // imageNodeOne.src = chosenProduct;
+  // parentEl.appendChild(imgEl);
   var randomNumberTwo = Math.floor(Math.random() * 20);
   while (randomNumberTwo == randomNumber) {
     var randomNumberTwo = Math.floor(Math.random() * 20);
   }
-  var chosenProduct = allProducts[randomNumberTwo].filepath;
-  var parentEl = document.getElementById('image-container');
-  var imgEl = document.createElement('img');
-  imgEl.src = chosenProduct;
-  parentEl.appendChild(imgEl);
+  var chosenProduct = allProducts[randomNumberTwo].filepath; //at the same time, increment here the number of times shown for each chosenProduct
+
+  allProducts[randomNumberTwo].numShown += 1;
+  // console.log(allProducts[randomNumberTwo].name + allProducts[randomNumberTwo].numShown);
+
+  // var parentEl = document.getElementById('image-container');
+  // var imgEl = document.createElement('img');
+  // var imageNodeTwo = document.getElementById('image-two');
+  imageNodeTwo.src = chosenProduct;
+  // parentEl.appendChild(imgEl);
   var randomNumberThree = Math.floor(Math.random() * 20);
   while (randomNumberThree == randomNumberTwo || randomNumberThree == randomNumber) {
     var randomNumberThree = Math.floor(Math.random() * 20);
   }
   var chosenProduct = allProducts[randomNumberThree].filepath;
-  var parentEl = document.getElementById('image-container');
-  var imgEl = document.createElement('img');
-  imgEl.src = chosenProduct;
-  parentEl.appendChild(imgEl);
+
+  allProducts[randomNumberThree].numShown += 1;
+  // console.log(allProducts[randomNumberThree].name + allProducts[randomNumberThree].numShown);
+
+  // var parentEl = document.getElementById('image-container');
+  // var imgEl = document.createElement('img');
+  // var imageNodeThree = document.getElementById('image-three');
+  imageNodeThree.src = chosenProduct;
+  // parentEl.appendChild(imgEl);
 }
 
-handleClicks();
-var placetoclick = document.getElementById('image-container');
-placetoclick.addEventListener('click', handleClicks);
+function handleClicks (event) {
+  console.log(event.target);
+  displayPics();
+}
+
+// var clickFirstImage = document.getElementById('image-one');
+
+displayPics();
+
+imageNodeOne.addEventListener('click', handleClicks);
+
+// var clickSecondImage = document.getElementById('image-two');
+imageNodeTwo.addEventListener('click', handleClicks);
+
+// var clickThirdImage = document.getElementById('image-three');
+imageNodeThree.addEventListener('click', handleClicks);
